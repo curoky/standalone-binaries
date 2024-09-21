2934d80b674e20019508207c4f511ea6ac235afe
curoky <cccuroky@gmail.com>
2024-09-21 14:58:33 +0800

    feat(git): fix git remote-https

--- pkgs/patched/git.nix
+++ pkgs/patched/git.nix
@@ -7,6 +7,7 @@
   nghttp2,
   libpsl,
   c-ares,
+  brotli,
 }:
 
 let
@@ -44,6 +45,7 @@ in
       nghttp2
       libpsl
       c-ares
+      brotli
     ];
 
     #doCheck = false;
@@ -51,13 +53,17 @@ in
 
     env.NIX_LDFLAGS =
       oldAttrs.env.NIX_LDFLAGS
-      + " -static -lnghttp2 -lnghttp3 -lcares -lngtcp2 -lngtcp2_crypto_ossl -lpsl -lssl -lcrypto -lssh2 -lidn2 -lzstd -lz -lunistring";
+      + " -static -lnghttp2 -lnghttp3 -lcares -lngtcp2 -lngtcp2_crypto_ossl -lpsl -lssl -lcrypto -lssh2 -lidn2 -lzstd -lz -lunistring -lbrotlidec -lbrotlicommon";
 
     patchPhase = ''
       find . -path './t/t[0-9][0-9][0-9][0-9]' -prune -o -type f -name '*.[ch]' -exec sed -i 's/\<error\>(/git_error(/g' {} +
       find . -path './t/t[0-9][0-9][0-9][0-9]' -prune -o -type f -name '*.[ch]' -exec sed -i 's/\<error\>\s\+(/git_error (/g' {} +
       find . -path './t/t[0-9][0-9][0-9][0-9]' -prune -o -type f -name '*.[ch]' -exec sed -i 's/int\s\+error\s*(/int git_error(/g' {} +
       find . -path './t/t[0-9][0-9][0-9][0-9]' -prune -o -type f -name '*.[ch]' -exec sed -i 's/undef error\b/undef git_error/' {} +
+    '';
+
+    preInstallCheck = oldAttrs.postInstall + ''
+      #disable_test t1517-outside-repo
     '';
 
     postInstall = (oldAttrs.postInstall or "") + ''
