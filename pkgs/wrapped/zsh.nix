7a68e6163ac7fb03b7dbfcee29a144aac2b51ee5
curoky <cccuroky@gmail.com>
2024-11-23 16:21:41 +0800

    fix(nix): resolve zsh static build issues and update package lists
    add fortify hardening disable to wrapped zsh derivation to fix GCC ICE in sort.o compilation
    fix zsh static package reference in flake.nix to use defaultPkgsStatic
    add nil package to linux pkgs list
    enable biome package in common.nix and remove unused entry from macos.nix

--- pkgs/wrapped/zsh.nix
+++ pkgs/wrapped/zsh.nix
@@ -12,13 +12,18 @@ let
 
     script_path="$(readlink -f "$0")"
     root=$(cd "$(dirname "$script_path")" && pwd)/..
-    export FPATH=$FPATH:$root/share/zsh/5.9/functions
+    export FPATH=$FPATH:$root/share/zsh/${zsh.version}/functions
 
     exec -a "$0" "$root/bin/_zsh" "$@"
   '';
 in
 
 zsh.overrideAttrs (oldAttrs: rec {
+  # Work around a GCC internal compiler error (ICE) in the object-size pass
+  # (compute_builtin_object_size) triggered while compiling Src/sort.o with
+  # _FORTIFY_SOURCE enabled for the static/musl build.
+  hardeningDisable = (oldAttrs.hardeningDisable or [ ]) ++ [ "fortify" ];
+
   patchPhase = oldAttrs.patchPhase or "" + ''
     echo "link=either" >> Src/Modules/system.mdd
     echo "link=either" >> Src/Modules/regex.mdd
