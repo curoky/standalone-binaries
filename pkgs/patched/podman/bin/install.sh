a784f0ff0913a2dbdc0fb50d93a653139d19a6c2
curoky <cccuroky@gmail.com>
2026-04-25 23:26:07 +0800

    feat(pkgs): podman update install.sh

--- pkgs/patched/podman/bin/install.sh
+++ pkgs/patched/podman/bin/install.sh
@@ -20,16 +20,18 @@ set -xeuo pipefail
 abspath=$(cd "$(dirname "$0")" && pwd)
 
 mkdir -p /opt/podmanx/
-cp -rf --remove-destination $abspath/../* /opt/podmanx/
+rm -rf /opt/podmanx/bin /opt/podmanx/conf /opt/podmanx/libexec
+cp -r $abspath/../bin $abspath/../conf $abspath/../libexec /opt/podmanx/
+chmod -R +w /opt/podmanx/bin /opt/podmanx/conf /opt/podmanx/libexec
 
 mkdir -p /etc/systemd/system/
-cp -rf --remove-destination ../conf/podmanxd.service /etc/systemd/system/podmanxd.service
+rm -rf /etc/systemd/system/podmanxd.service
+cp $abspath/../conf/podmanxd.service /etc/systemd/system/podmanxd.service
 
 systemctl daemon-reload
 systemctl enable podmanxd.service
 systemctl start podmanxd.service
 systemctl status podmanxd.service
-chmod +777 /tmp/podmanx.sock
 
 # wget https://github.com/curoky/devspace/releases/download/v1.0/podman.tar
 # tar -x -f podman.tar
