2836ba6bd1ea14af5f8aaa6e00924133816e2f70
curoky <cccuroky@gmail.com>
2026-06-24 15:08:28 +0800

    build(vim-plugins): comment out delimitMate, vim-commentary and vim-fugitive
    comment out their inherit declarations and installation copy commands to exclude the plugins from the package

--- pkgs/vim-plugins.nix
+++ pkgs/vim-plugins.nix
@@ -6,13 +6,13 @@
 
 let
   inherit (vimPlugins) vim-plug;
-  inherit (vimPlugins) delimitMate;
+  # inherit (vimPlugins) delimitMate;
   inherit (vimPlugins) vim-airline;
   inherit (vimPlugins) vim-airline-themes;
   inherit (vimPlugins) vim-colors-solarized;
   inherit (vimPlugins) indentLine;
-  inherit (vimPlugins) vim-commentary;
-  inherit (vimPlugins) vim-fugitive;
+  # inherit (vimPlugins) vim-commentary;
+  # inherit (vimPlugins) vim-fugitive;
   inherit (vimPlugins) vim-gitgutter;
   inherit (vimPlugins) nerdtree;
   inherit (vimPlugins) nerdtree-git-plugin;
@@ -35,13 +35,13 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/vim-plugin/autoload
     cp ${vim-plug.src}/plug.vim $out/share/vim-plugin/autoload/
     mkdir -p $out/share/vim-plugin/plugged
-    cp -r ${delimitMate}/ $out/share/vim-plugin/plugged/delimitMate
+    # cp -r ${delimitMate}/ $out/share/vim-plugin/plugged/delimitMate
     cp -r ${vim-airline}/ $out/share/vim-plugin/plugged/vim-airline
     cp -r ${vim-airline-themes}/ $out/share/vim-plugin/plugged/vim-airline-themes
     cp -r ${vim-colors-solarized}/ $out/share/vim-plugin/plugged/vim-colors-solarized
     cp -r ${indentLine}/ $out/share/vim-plugin/plugged/indentLine
-    cp -r ${vim-commentary}/ $out/share/vim-plugin/plugged/vim-commentary
-    cp -r ${vim-fugitive}/ $out/share/vim-plugin/plugged/vim-fugitive
+    # cp -r ${vim-commentary}/ $out/share/vim-plugin/plugged/vim-commentary
+    # cp -r ${vim-fugitive}/ $out/share/vim-plugin/plugged/vim-fugitive
     cp -r ${vim-gitgutter}/ $out/share/vim-plugin/plugged/vim-gitgutter
     cp -r ${nerdtree}/ $out/share/vim-plugin/plugged/nerdtree
     cp -r ${nerdtree-git-plugin}/ $out/share/vim-plugin/plugged/nerdtree-git-plugin
